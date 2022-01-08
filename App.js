90b932d142b83973bb840412aedb46a815ce68ff
Chelsea Farley <chelseafarley91@gmail.com>
2022-01-08 22:43:51 +1300

    Pressable demo

--- App.js
+++ App.js
@@ -1,10 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Pressable } from 'react-native';
 
 export default function App() {
+  const logPress = (pressType) => {
+    console.log(pressType);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Pressable 
+        onPress={() => logPress("onPress")} 
+        onPressIn={() => logPress("onPressIn")} 
+        onPressOut={() => logPress("onPressOut")} 
+        onLongPress={() => logPress("onLongPress")}
+        style={({ pressed }) => [
+          {
+            opacity: pressed
+              ? 0.5
+              : 1
+          }
+        ]}>
+        <Text>Press Me!</Text>
+      </Pressable>
       <StatusBar style="auto" />
     </View>
   );
